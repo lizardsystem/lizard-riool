c091205dad6943adfe730115d7b5946fe34fa7f2
byrman <carsten.byrman@nelen-schuurmans.nl>
2012-03-08 13:39:16 +0000

    Each page has its own .js file.

---
+++ lizard_riool/media/lizard_riool/beheer.js
@@ -0,0 +1,24 @@
+// (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
+
+// jslint configuration
+/*global $ */
+
+"use strict";
+
+$(function () {
+
+    var $dialog;
+
+    $dialog = $('<div class="upload-dialog"/>').load('/riolering/upload/').dialog({
+        autoOpen: false,
+        title: 'Uploaden',
+        width: 500
+    });
+
+    $('.upload').click(function () {
+        $dialog.dialog('open');
+        // prevent the default action, e.g., following a link
+        return false;
+    });
+
+});
--- lizard_riool/media/lizard_riool/side_profile.js
+++ lizard_riool/media/lizard_riool/side_profile.js
@@ -5,24 +5,6 @@
 
 "use strict";
 
-$(function () {
-
-    var $dialog;
-
-    $dialog = $('<div class="upload-dialog"/>').load('/riolering/upload/').dialog({
-        autoOpen: false,
-        title: 'Uploaden',
-        width: 500
-    });
-
-    $('.upload').click(function () {
-        $dialog.dialog('open');
-        // prevent the default action, e.g., following a link
-        return false;
-    });
-
-});
-
 function draw_path(putten) {
 
     var feature, i, layer, layers, line, point, points, put;
